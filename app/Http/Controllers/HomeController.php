a38c8df64175c63786199dfee72c757dd6cb114e
sindyalvinaa <sindyalvinaa93@gmail.com>
2022-04-26 11:26:47 +0700

    Fourth Commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * Codekop Toko Online
- * 
- * @link       https://www.codekop.com/
- * @version    1.0.1
- * @copyright  (c) 2021 
- * 
- * File      : HomeController.php
- * Web Name  : Toko Online
- * Developer : Fauzan Falah 
- * E-mail    : fauzancodekop@gmail.com / fauzan1892@codekop.com
- * 
- * 
-**/
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -30,7 +17,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
 
-        $reqsearch = $request->get('search');  
+        $reqsearch = $request->get('search');
         $produkdb = Produk::leftJoin('kategori','produk.id_kategori','=','kategori.id')
                     ->select('kategori.nama_kategori','produk.*');
         $data = [
@@ -56,7 +43,7 @@ class HomeController extends Controller
 
     public function search(Request $request)
     {
-        $reqsearch = $request->get('keyword');  
+        $reqsearch = $request->get('keyword');
         $produkdb = Produk::leftJoin('kategori','produk.id_kategori','=','kategori.id')
             ->select('kategori.nama_kategori','produk.*')
             ->when($reqsearch, function($query, $reqsearch){
@@ -75,7 +62,7 @@ class HomeController extends Controller
 
     public function produk(Request $request, $id)
     {
-        $reqsearch = $request->get('keyword');  
+        $reqsearch = $request->get('keyword');
         $produkdb = Produk::leftJoin('kategori','produk.id_kategori','=','kategori.id')
             ->select('kategori.nama_kategori','produk.*')->where('produk.id', $id)->first();
 
